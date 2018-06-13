4789b151fb61a14804c9997af14c34fc46754d2e
Marco Barcellos <m.barcellos@decentral.ca>
2018-06-12 23:21:13 -0400

    minor tweak on getListing test

--- test/contract-service.test.js
+++ test/contract-service.test.js
@@ -75,7 +75,7 @@ describe('ContractService', function() {
   describe('getListing', () => {
     // Skipped because of https://github.com/OriginProtocol/platform/issues/27
     it('should reject when listing cannot be found', async () => {
-      await expect(contractService.getListing('foo')).to.throw
+      await expect(contractService.getListing('foo')).to.throw()
     })
 
     it('should get a listing object', async () => {
